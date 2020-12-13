f09a72c7159d4bfd0cb0d7604f0858afe270c068
Luca Ban <mesqueeb@gmail.com>
2020-12-13 13:59:02 +0900

    test: add test

--- test/compareObjectsBasedOn.ts
+++ test/compareObjectsBasedOn.ts
@@ -11,6 +11,25 @@ test('compareObjectsBasedOn', t => {
   t.deepEqual(result.equal, false)
 })
 
+// I don't think I want this syntax because it's too complex:
+// test('compareObjectsBasedOn - nested', t => {
+//   const base = { nested: { a: 1, b: 2 } }
+//   const check = { nested: { a: 1, b: 3 } }
+//   const result = compareObjectsBasedOn(['nested.a', 'nested.b'] as any[], [base, check])
+//   t.deepEqual(result.differentProps, ['nested.b'])
+//   t.deepEqual(result.differentPropsPicked, [{ b: 2 }, { b: 3 }] as any)
+//   t.deepEqual(result.equal, false)
+// })
+
+test('compareObjectsBasedOn - check nested props flat', t => {
+  const base = { nested: { a: 1, b: 2 } }
+  const check = { nested: { a: 1, b: 3 } }
+  const result = compareObjectsBasedOn(['nested'], [base, check])
+  t.deepEqual(result.differentProps, ['nested'])
+  t.deepEqual(result.differentPropsPicked, [{ nested: { a: 1, b: 2 } }, { nested: { a: 1, b: 3 } }])
+  t.deepEqual(result.equal, false)
+})
+
 test('compareObjectsBasedOn - ok', t => {
   const base = { a: 1, b: true, c: 'CC' }
   const check = { a: 1, c: 'CC', d: 1 }
