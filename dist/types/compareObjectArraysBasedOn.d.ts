38e9fe41a2afa64ea34a007f1607aca9ab18f9d0
Luca Ban <mesqueeb@gmail.com>
2023-05-23 11:20:45 +0900

    fix: exports

--- dist/types/compareObjectArraysBasedOn.d.ts
+++
@@ -1,6 +0,0 @@
-import { ComparisonObjects } from './compareObjectsBasedOn';
-export type ComparisonObjectArrays<ObjectType> = {
-    perIndex: ComparisonObjects<ObjectType>[];
-    equal: boolean;
-};
-export declare function compareObjectArraysBasedOn<ObjectType extends Record<string, any> | any = Record<string, any>>(propKeys: (keyof ObjectType)[], objectArrays: [[ObjectType, ...any[]], ...any[]]): ComparisonObjectArrays<ObjectType>;
--- dist/types/compareObjectsBasedOn.d.ts
+++
@@ -1,6 +0,0 @@
-export type ComparisonObjects<ObjectType> = {
-    differentProps: (keyof ObjectType)[];
-    differentPropsPicked: Partial<ObjectType>[];
-    equal: boolean;
-};
-export declare function compareObjectsBasedOn<ObjectType extends Record<string, any> | any = Record<string, any>>(propKeys: (keyof ObjectType)[], objects: [ObjectType, ...any[]]): ComparisonObjects<ObjectType>;
